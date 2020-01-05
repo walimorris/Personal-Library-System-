6296d83ef4910c6564a5f67ee3c8c68a430fa205
walimorris <48896622+walimorris@users.noreply.github.com>
2020-01-05 09:57:26 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+!/bin/bash
 
+printf "Welcome to the Home Library system! Are you a first time user?\n"
+printf "[WARNING!] Selecting 'Y' will update your system, install dialog and set-up this program. Continue? Y/n "
 
+read -r setup
+
+if [ $setup = 'Y' ] || [ $setup = 'y' ]; then
+    sudo apt-get install python3-dialog
+    ln -s startup.sh Library
+fi
+
+printf "The Home Library is setup on this system. From this point you can run ./Library from this directory to run this program.\n"
